9758bed7b6c7778223cf2811ea288b7aee9617fa
Nick Dimitriou <81875532+NiDimi@users.noreply.github.com>
2025-10-03 18:28:23 +0100

    Add Initial Contracts for Soroban (#16)
    * Initialising with Stellar contract command
    
    * Events and nullifier functionality
    
    * Modified to basic contract functionality for now
    
    * Removing snapsots
    
    * Modifing gitignore for snapsots
    
    * Update Cargo.toml
    
    * Fix CLI
    
    * Update Cargo.toml
    
    * Moved allowed licence becasue of wasmparser to exceptions
    
    * Address PR comments
    
    * Fix CI
    
    * Updated from Main to resolve conflicts
    
    * Changed H32 name + disabled miri
    
    -> Changed the H32 name to a more general HashBytes because we might change 32 in the future
    -> Miri is not working properly with the Soroban SDK so until its fixed I commented out the check
    
    * Modifying allow licence

---
+++ contracts/pool/src/lib.rs
@@ -0,0 +1,67 @@
+#![no_std]
+
+use soroban_sdk::{
+    Address, Bytes, BytesN, Env, Map, Symbol, U256, Vec, contract, contractevent, contractimpl,
+    contracttype, symbol_short,
+};
+
+#[contract]
+pub struct PoolContract;
+
+const COMMITMENTS: Symbol = symbol_short!("commits");
+const NULLIFIERS: Symbol = symbol_short!("nullifs");
+
+pub const HASH_SIZE: usize = 32;
+type HashBytes = BytesN<HASH_SIZE>;
+
+#[contracttype]
+pub struct Proof {
+    pub proof: Bytes,
+    pub root: HashBytes,
+    pub input_nullifiers: Vec<HashBytes>,
+    pub output_commitment0: HashBytes,
+    pub output_commitment1: HashBytes,
+    pub public_amount: U256,
+    pub ext_data_hash: HashBytes,
+}
+
+#[contractevent(topics = ["withdraw"], data_format = "single-value")]
+struct WithdrawEvent {
+    address: Address,
+}
+
+#[contractimpl]
+impl PoolContract {
+    pub fn deposit(env: Env, _from: Address, commitment: BytesN<32>, proof: Proof) {
+        if proof.proof.is_empty() {
+            panic!("invalid proof");
+        }
+        // Verify proof
+
+        let mut commits: Vec<BytesN<32>> = env
+            .storage()
+            .instance()
+            .get(&COMMITMENTS)
+            .unwrap_or(Vec::new(&env));
+        commits.push_back(commitment);
+        env.storage().instance().set(&COMMITMENTS, &commits);
+    }
+
+    pub fn withdraw(env: Env, to: Address, nullifier: BytesN<32>) {
+        // Prevent double-withdraw
+        let mut nullifs: Map<BytesN<32>, bool> = env
+            .storage()
+            .instance()
+            .get(&NULLIFIERS)
+            .unwrap_or(Map::new(&env));
+        if nullifs.contains_key(nullifier.clone()) {
+            panic!("nullifier already used");
+        }
+        nullifs.set(nullifier, true);
+        env.storage().instance().set(&NULLIFIERS, &nullifs);
+
+        WithdrawEvent { address: to }.publish(&env);
+    }
+}
+
+mod test;
