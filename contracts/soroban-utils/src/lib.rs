870642793f5f10e363b07f5d98dd3270263f7838
Antonio <Fantoni0@users.noreply.github.com>
2025-12-09 08:41:48 +0100

    Feature/asp contracts (#37)
    * Initial version of ASP contract
    
    * Add zeroes
    
    * Working version of ASP
    
    * Fix errors on merkle tree
    
    * Extract deps to shared crate
    
    * Fix merge conflicts
    
    * Address PR comments
    
    * ASP contract for Non-membership (#39)
    
    * Working non-membership ASP
    
    * Minor edit in SMT commments
    
    * Fix conflicts
    
    * Address PR comments
    
    * Address PR comments

---
+++ contracts/soroban-utils/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+//! Shared utilities for Soroban contracts
+//!
+//! This crate provides common functions and constants that can be reused
+//! across multiple Soroban contracts
+
+pub mod constants;
+pub mod poseidon2;
+pub mod utils;
+
+pub use constants::*;
+pub use poseidon2::*;
+pub use utils::*;
