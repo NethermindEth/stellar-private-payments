2a0ac3955d212cc9479294c0f34a235a17eed1b1
Fantoni0 <antoniomlf@protonmail.com>
2026-01-02 16:45:56 +0100

    Fmt

--- app/src/merkle.rs
+++ app/src/merkle.rs
@@ -69,7 +69,8 @@ pub struct MerkleTree {
     next_index: u64,
 }
 
-// TODO: For now we implement a full merkle tree for quick prototyping. We should implement a partial merkle tree next to minimize storage on user side
+// TODO: For now we implement a full merkle tree for quick prototyping. We
+// should implement a partial merkle tree next to minimize storage on user side
 
 #[wasm_bindgen]
 impl MerkleTree {
