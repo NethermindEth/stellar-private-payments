e760dae5675ce4af83c895a3f02c4b134935785a
Nick Dimitriou <moonkace24@gmail.com>
2026-01-15 17:06:45 +0000

    PR requests

--- app/js/__mocks__/prover.js
+++ app/js/__mocks__/prover.js
@@ -38,52 +38,8 @@ class MerkleTree {
 
 class MerkleProof {}
 
-function derive_public_key(bytes) {
-  return new Uint8Array(bytes);
-}
-
-function derive_public_key_hex() {
-  return '0xdeadbeef';
-}
-
-function compute_commitment() {
-  return new Uint8Array([6]);
-}
-
-function compute_signature() {
-  return new Uint8Array([7]);
-}
-
-function compute_nullifier() {
-  return new Uint8Array([8]);
-}
-
-function poseidon2_hash2() {
-  return new Uint8Array([9]);
-}
-
-function poseidon2_hash3() {
-  return new Uint8Array([10]);
-}
-
 function u64_to_field_bytes() {
   return new Uint8Array([11]);
-}
-
-function decimal_to_field_bytes() {
-  return new Uint8Array([12]);
-}
-
-function hex_to_field_bytes() {
-  return new Uint8Array([13]);
-}
-
-function field_bytes_to_hex() {
-  return '0x0d';
-}
-
-function verify_proof() {
-  return true;
 }
 
 function version() {
@@ -96,17 +52,6 @@ module.exports = {
   Prover,
   MerkleTree,
   MerkleProof,
-  derive_public_key,
-  derive_public_key_hex,
-  compute_commitment,
-  compute_signature,
-  compute_nullifier,
-  poseidon2_hash2,
-  poseidon2_hash3,
   u64_to_field_bytes,
-  decimal_to_field_bytes,
-  hex_to_field_bytes,
-  field_bytes_to_hex,
-  verify_proof,
   version,
 };
