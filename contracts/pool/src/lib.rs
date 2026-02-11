0fc07858de8f753aa6ab90df530af0ae4d662300
Musa AbdulKareem <wisemrmusa@gmail.com>
2026-02-11 16:44:30 +0100

    fix: lessen lint rules for contracts

--- contracts/pool/src/lib.rs
+++ contracts/pool/src/lib.rs
@@ -1,4 +1,10 @@
+//! Privacy pool contract for Stellar private transactions.
 #![no_std]
+#![allow(
+    missing_docs,
+    clippy::arithmetic_side_effects,
+    clippy::cast_possible_truncation
+)]
 
 pub mod merkle_with_history;
 pub mod pool;
