e6f06b4fd00d70f5d5ae33dfae19c99f32ce8621
Musa AbdulKareem <wisemrmusa@gmail.com>
2026-02-12 10:09:03 +0100

    test: ignore playwright test in jest

--- app/jest.config.cjs
+++ app/jest.config.cjs
@@ -1,11 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: "node",
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    "^.+\\.js$": "babel-jest",
   },
   clearMocks: true,
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/e2e/"],
   moduleNameMapper: {
-    '^\\./prover\\.js$': '<rootDir>/js/__mocks__/prover.js',
-    '^\\./prover-client\\.js$': '<rootDir>/js/__mocks__/prover-client.js',
+    "^\\./prover\\.js$": "<rootDir>/js/__mocks__/prover.js",
+    "^\\./prover-client\\.js$": "<rootDir>/js/__mocks__/prover-client.js",
   },
 };
