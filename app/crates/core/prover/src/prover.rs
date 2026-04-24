3b34efe9389b501db97c9504571c76ab60e0ec93
Maksim Ryndin <maksim.ryndin@nethermind.io>
2026-04-24 15:42:28 +0000

    circuit reduction
    Co-authored-by: Antonio <Fantoni0@users.noreply.github.com>

--- app/crates/core/prover/src/prover.rs
+++ app/crates/core/prover/src/prover.rs
@@ -383,10 +383,15 @@ impl Prover {
         };
 
         let mut rng = OsRng;
+<<<<<<< HEAD
         let proof = <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::prove(
             &self.pk, circuit, &mut rng,
         )
         .map_err(|e| anyhow!("Proof generation failed: {}", e))?;
+=======
+        let proof = <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::prove(&self.pk, circuit, &mut rng)
+            .map_err(|e| anyhow!("Proof generation failed: {}", e))?;
+>>>>>>> 7e7aa3e (circuit reduction)
 
         Ok(proof_to_uncompressed_bytes(&proof))
     }
@@ -465,6 +470,7 @@ impl Prover {
         }
 
         // Verify
+<<<<<<< HEAD
         let result =
             <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::verify_with_processed_vk(
                 &self.pvk,
@@ -472,6 +478,14 @@ impl Prover {
                 &proof,
             )
             .map_err(|e| anyhow!("Verification error: {}", e))?;
+=======
+        let result = <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::verify_with_processed_vk(
+            &self.pvk,
+            &public_inputs,
+            &proof,
+        )
+        .map_err(|e| anyhow!("Verification error: {}", e))?;
+>>>>>>> 7e7aa3e (circuit reduction)
 
         Ok(result)
     }
@@ -517,6 +531,7 @@ pub fn verify_proof(
     }
 
     // Verify
+<<<<<<< HEAD
     let result =
         <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::verify_with_processed_vk(
             &pvk,
@@ -524,6 +539,14 @@ pub fn verify_proof(
             &proof,
         )
         .map_err(|e| anyhow!("Verification error: {}", e))?;
+=======
+    let result = <ark_groth16::Groth16<Bn254, CircomReduction> as SNARK<Fr>>::verify_with_processed_vk(
+        &pvk,
+        &public_inputs,
+        &proof,
+    )
+    .map_err(|e| anyhow!("Verification error: {}", e))?;
+>>>>>>> 7e7aa3e (circuit reduction)
 
     Ok(result)
 }
